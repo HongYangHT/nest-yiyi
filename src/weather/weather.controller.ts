01c65cd9333c744263c6062c71461a89379d469e
hongyang <hongyang@asiainfo.com>
2020-06-10 20:00:02 +0800

    fixed:修改打印日志

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -3,24 +3,38 @@
  * @LastEditors: sam.hongyang
  * @Description: function description
  * @Date: 2020-06-10 17:51:03
- * @LastEditTime: 2020-06-10 18:44:36
+ * @LastEditTime: 2020-06-10 19:58:40
  */ 
 import { Controller, UseGuards, Get, Query, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import axios from 'axios';
+import { MyLoggerService } from '../utils/log';
 
 @Controller('weather')
 @UseGuards(AuthGuard('jwt'))
 export class WeatherController {
+    constructor(
+        private readonly myLoggerService: MyLoggerService,
+    ) {}
     @Get('fetch')
     async fetchWeather(@Query() query, @Request() request) {
+        const ip = request.headers['X-Real-IP'] ||
+        request.headers['x-forwarded-for'] ||
+        request.connection.remoteAddress ||
+        request.socket.remoteAddress ||
+        request.connection.socket.remoteAddress;
         const result = await axios.get('https://tianqiapi.com/free/day', {
             params: {
                 ...query,
-                ip: request.ip,
+                ip,
             },
         });
-
+        this.myLoggerService.write({
+            params: {
+                ...query,
+                ip,
+            },
+        });
         return result.data;
     }
 }
--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -3,12 +3,14 @@
  * @LastEditors: sam.hongyang
  * @Description: function description
  * @Date: 2020-06-10 17:50:40
- * @LastEditTime: 2020-06-10 17:52:56
+ * @LastEditTime: 2020-06-10 19:57:37
  */ 
 import { Module } from '@nestjs/common';
 import { WeatherController } from './weather.controller';
+import { MyLoggerService } from '../utils/log';
 
 @Module({
     controllers: [WeatherController],
+    providers: [MyLoggerService],
 })
 export class WeatherModule {}
