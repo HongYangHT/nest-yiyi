feeb8e4f822b862d6f5e099fcbd0d576e0d134a0
hongyang <hongyang@asiainfo.com>
2020-06-10 18:48:37 +0800

    feat:修改天气接口中提交请求ip

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -3,9 +3,9 @@
  * @LastEditors: sam.hongyang
  * @Description: function description
  * @Date: 2020-06-10 17:51:03
- * @LastEditTime: 2020-06-10 18:02:26
+ * @LastEditTime: 2020-06-10 18:44:36
  */ 
-import { Controller, UseGuards, Get, Query } from '@nestjs/common';
+import { Controller, UseGuards, Get, Query, Request } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import axios from 'axios';
 
@@ -13,10 +13,11 @@ import axios from 'axios';
 @UseGuards(AuthGuard('jwt'))
 export class WeatherController {
     @Get('fetch')
-    async fetchWeather(@Query() query) {
+    async fetchWeather(@Query() query, @Request() request) {
         const result = await axios.get('https://tianqiapi.com/free/day', {
             params: {
                 ...query,
+                ip: request.ip,
             },
         });
 
