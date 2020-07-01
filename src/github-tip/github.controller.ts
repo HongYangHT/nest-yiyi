f5ee1e6bf4ae93d9e7df40a0cef30ba4c5cd1caf
hongyang <hongyang@asiainfo.com>
2020-07-01 17:08:07 +0800

    feat:打印自动部署日志

--- src/github-tip/github.controller.ts
+++ src/github-tip/github.controller.ts
@@ -3,12 +3,13 @@
  * @LastEditors: sam.hongyang
  * @Description: function description
  * @Date: 2020-07-01 11:51:02
- * @LastEditTime: 2020-07-01 14:59:35
+ * @LastEditTime: 2020-07-01 17:07:45
  */ 
 import { Controller, Post, Res, HttpStatus } from '@nestjs/common';
 import { MyLoggerService } from '../utils/log';
 import { spawn } from 'child_process';
 import createHandler from 'github-webhook-handler';
+import { Logger } from '../utils/logical';
 
 @Controller('github')
 export class GithubController {
@@ -21,8 +22,12 @@ export class GithubController {
             const handler = createHandler({ path: '/api/v1/github/tip', secret: 'Hong@410g' });
             handler.on('error', err => {
                 this.myLoggerService.write(`Error: ${ err.message }`);
+                Logger.info(`Error: ${ err.message }`);
             });
             handler.on('push', event => {
+                Logger.info(`Received a push event for %s to %s'
+                ${ event.payload.repository.name }
+                ${ event.payload.ref }`);
                 this.myLoggerService.write(`Received a push event for %s to %s'
                     ${ event.payload.repository.name }
                     ${ event.payload.ref }`);
@@ -31,18 +36,27 @@ export class GithubController {
                         cwd: `/root/workspace/${event.payload.repository.name}`,
                     });
                     s.stdout.on('data', (data) => {
+                        Logger.info(`${event.payload.repository.name}: ${data}`);
                         this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
                     });
                     s.stderr.on('data', (data) => {
+                        Logger.info(`${event.payload.repository.name}: ${data}`);
                         this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
                     });
+                    Logger.info(`${event.payload.repository.name} has rebuild`);
                     this.myLoggerService.write(`${event.payload.repository.name} has rebuild`);
                 } catch (e) {
                     this.myLoggerService.write(e);
+                    Logger.info(e);
                 }
             });
             handler.on('issues', event => {
                 this.myLoggerService.write(`Received an issue event for %s action=%s: #%d %s',
+                ${event.payload.repository.name}
+                ${event.payload.action}
+                ${event.payload.issue.number}
+                ${event.payload.issue.title}`);
+                Logger.info(`Received an issue event for %s action=%s: #%d %s',
                 ${event.payload.repository.name}
                 ${event.payload.action}
                 ${event.payload.issue.number}
