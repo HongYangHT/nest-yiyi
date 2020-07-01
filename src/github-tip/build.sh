00ce86a3a9bf5ff1bf789c7dabac35634dbd77e9
hongyang <hongyang@asiainfo.com>
2020-07-01 15:00:47 +0800

    fixed:修改请求参数

--- src/github-tip/build.sh
+++ src/github-tip/build.sh
@@ -3,12 +3,19 @@
  # @LastEditors: sam.hongyang
  # @Description: function description
  # @Date: 2020-07-01 12:10:27
- # @LastEditTime: 2020-07-01 12:13:53
+ # @LastEditTime: 2020-07-01 13:53:15
 ### 
+echo "start deployment"
+# WEB_PATH='/root/workspace'
+# cd $WEB_PATH
+
+echo "fetching from remote..."
 git reset --hard origin/master
 git clean -f
 git pull
 npm install
+
+echo "start build"
 npm run build:prod
 
 echo 'build successfully'
--- src/github-tip/github.controller.ts
+++ src/github-tip/github.controller.ts
@@ -3,9 +3,9 @@
  * @LastEditors: sam.hongyang
  * @Description: function description
  * @Date: 2020-07-01 11:51:02
- * @LastEditTime: 2020-07-01 12:27:44
+ * @LastEditTime: 2020-07-01 14:59:35
  */ 
-import { Controller, Post, Res } from '@nestjs/common';
+import { Controller, Post, Res, HttpStatus } from '@nestjs/common';
 import { MyLoggerService } from '../utils/log';
 import { spawn } from 'child_process';
 import createHandler from 'github-webhook-handler';
@@ -16,40 +16,40 @@ export class GithubController {
         private readonly myLoggerService: MyLoggerService,
     ) {}
     @Post('tip')
-    async postTip(@Res() res) {
-        const handler = createHandler({ path: '/', secret: 'Hong@410g' });
-        handler.on('error', err => {
-            this.myLoggerService.write(`Error: ${ err.message }`);
+    async postTip() {
+        await new Promise((resolve, reject) => {
+            const handler = createHandler({ path: '/api/v1/github/tip', secret: 'Hong@410g' });
+            handler.on('error', err => {
+                this.myLoggerService.write(`Error: ${ err.message }`);
+            });
+            handler.on('push', event => {
+                this.myLoggerService.write(`Received a push event for %s to %s'
+                    ${ event.payload.repository.name }
+                    ${ event.payload.ref }`);
+                try {
+                    const s = spawn('sh', ['./build.sh'], {
+                        cwd: `/root/workspace/${event.payload.repository.name}`,
+                    });
+                    s.stdout.on('data', (data) => {
+                        this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
+                    });
+                    s.stderr.on('data', (data) => {
+                        this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
+                    });
+                    this.myLoggerService.write(`${event.payload.repository.name} has rebuild`);
+                } catch (e) {
+                    this.myLoggerService.write(e);
+                }
+            });
+            handler.on('issues', event => {
+                this.myLoggerService.write(`Received an issue event for %s action=%s: #%d %s',
+                ${event.payload.repository.name}
+                ${event.payload.action}
+                ${event.payload.issue.number}
+                ${event.payload.issue.title}`);
+            });
+            resolve();
         });
-        handler.on('push', event => {
-            this.myLoggerService.write(`Received a push event for %s to %s'
-                ${ event.payload.repository.name }
-                ${ event.payload.ref }`);
-            try {
-                const s = spawn('sh', ['./build.sh'], {
-                    cwd: `/root/workspace/${event.payload.repository.name}`,
-                });
-                s.stdout.on('data', (data) => {
-                    this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
-                });
-                s.stderr.on('data', (data) => {
-                    this.myLoggerService.write(`${event.payload.repository.name}: ${data}`);
-                });
-                this.myLoggerService.write(`${event.payload.repository.name} has rebuild`);
-            } catch (e) {
-                this.myLoggerService.write(e);
-            }
-        });
-        handler.on('issues', event => {
-            this.myLoggerService.write(`Received an issue event for %s action=%s: #%d %s',
-            ${event.payload.repository.name}
-            ${event.payload.action}
-            ${event.payload.issue.number}
-            ${event.payload.issue.title}`);
-        });
-
-        return {
-            data: '支撑成功',
-        }
+        return { message: '更新成功' };
     }
 }
